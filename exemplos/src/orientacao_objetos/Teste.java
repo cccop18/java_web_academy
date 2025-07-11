1bee380c9389bb4c85fd6537b06652af4b5e69e5
Manoel <mlimeira@ic.uff.br>
2025-07-11 13:56:21 -0500

    Conceito de Classes e Objetos

---
+++ exemplos/src/orientacao_objetos/Teste.java
@@ -0,0 +1,16 @@
+package orientacao_objetos;
+
+public class Teste {
+    public static void main(String[] args) {
+        // Criação de um objeto do tipo Pessoa
+        Pessoa vinicius = new Pessoa();
+        // Não podemos acessar diretamente os atributos privados
+        // vinicius.nome = "Vinicius Barros de Melo";
+        // vinicius.email = "vinicius@ufac.br";
+        // vinicius.telefone = "12345678901";
+        // System.out.println(vinicius.nome);
+        vinicius.setNome("Vinicius Barros de Melo");
+        System.out.println(vinicius.getNome());
+    }
+
+}
