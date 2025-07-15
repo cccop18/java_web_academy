7ab96608ff702657cb8b6fc002166c193aae2615
Manoel <mlimeira@ic.uff.br>
2025-07-14 20:12:50 -0500

    Conexão com Banco

--- sgcm/src/main/java/br/ufac/sgcm/model/Teste.java
+++ sgcm/src/main/java/br/ufac/sgcm/model/Teste.java
@@ -1,9 +1,22 @@
 package br.ufac.sgcm.model;
 
+import java.sql.Connection;
+
+import br.ufac.sgcm.dao.ConexaoDB;
+
 public class Teste {
     public static void main(String[] args) {
         Especialidade e1 = new Especialidade();
         e1.setNome("Cardiologista");
         System.out.println(e1.getNome());
+
+        // Criar um objeto da classe ConxeaoDB
+        ConexaoDB conexao = new ConexaoDB();
+        Connection instancia = conexao.getConexao();
+        if (instancia != null)
+            System.out.println("Conexão realizada com sucesso!");
+        else
+            System.out.println("Falha na conexão.");
+
     }
 }
